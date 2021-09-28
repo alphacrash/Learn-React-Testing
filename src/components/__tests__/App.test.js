fdce3f4de891a634af7fc88696dc81c1427fa1d1
alphacrash <sudhansu.ace@gmail.com>
2021-09-29 00:06:58 +0530

    Refactor and new test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "../App";
 
-test("renders expects hi", () => {
+it("shows a comment box", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain("Hi there!");
+  expect(div.innerHTML).toContain("Comment Box");
   ReactDOM.unmountComponentAtNode(div);
 });
